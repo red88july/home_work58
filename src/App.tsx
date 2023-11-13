cfdc8a0038f29786edc58521f1bbea74b0865854
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-11-13 19:12:30 +0600

    "Set attribute type for alert, after that made style for bg alert using switch/case"

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,53 @@
 import Toolbar from './components/Toolbar/Toolbar';
 import Modal from './components/Modal/Modal';
 import Alert from './components/Alert/Alert';
-import {useState} from 'react';
+import React, {useState} from 'react';
 
 
 function App() {
-    const [showModal, setShowModal] = useState(false);
-    return (
-        <>
-            <header>
-                <Toolbar/>
-            </header>
-            <main className="container-fluid">
-                <div className="text-center">
-                    <button className="btn btn-primary mt-5 w-25" onClick={() => setShowModal(true)}>Show modal window
-                    </button>
-                </div>
-                <Modal show={showModal} title="Demonstration modal windows" onClose={() => setShowModal(false)}>
-                    <div className="modal-body">
-                        <p>We retain information relating to the Services until it is no longer necessary to serve the
-                            purposes for which it was collected,
-                            such as to provide the Services to you or improve the Services.
-                            Reasons we may retain some information for longer periods include:
-                        </p>
-                    </div>
-                </Modal>
-              <div className="text-center d-flex justify-content-center align-items-center flex-column">
-                <button className="btn btn-info mt-3 w-25">Show alert</button>
-                <Alert />
-              </div>
-            </main>
-        </>
-    );
+  const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+
+  return (
+    <>
+      <header>
+        <Toolbar/>
+      </header>
+      <main className="container-fluid">
+        <div className="text-center">
+          <button className="btn btn-primary mt-5 w-25" onClick={() => setShowModal(true)}>Show modal window
+          </button>
+        </div>
+        <Modal show={showModal} title="Demonstration modal windows" onClose={() => setShowModal(false)}>
+          <div className="modal-body">
+            <p>We retain information relating to the Services until it is no longer necessary to serve the
+              purposes for which it was collected,
+              such as to provide the Services to you or improve the Services.
+              Reasons we may retain some information for longer periods include:
+            </p>
+          </div>
+        </Modal>
+        <div className="text-center d-flex justify-content-center align-items-center flex-column">
+          <button className="btn btn-info mt-3 w-25"
+                  onClick={() => setShowAlert(true)}>Show alert
+          </button>
+          <Alert
+            type="alert-primary"
+            alert={showAlert}>This is a primary alert </Alert>
+          <Alert
+            type="alert-success"
+            alert={showAlert}>This is a success alert</Alert>
+          <Alert
+            type="alert-warning"
+            alert={showAlert}>This is a warning alert</Alert>
+          <Alert
+            type="alert-danger"
+            alert={showAlert}>This is a danger alert</Alert>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-const Alert: React.FC = () => {
+interface Props extends React.PropsWithChildren{
+  alert:boolean,
+  type: 'alert-primary' | 'alert-success' | 'alert-danger' | 'alert-warning',
+}
+
+const Alert: React.FC<Props> = ({alert, type, children}) => {
+
+  const backgrondColor = (type: string) => {
+    switch (type){
+      case'alert-primary':
+        return 'var(--bs-primary-bg-subtle)';
+      case'alert-success':
+        return 'var(--bs-success-bg-subtle)';
+      case'alert-warning':
+        return 'var(--bs-warning-bg-subtle)';
+      case'alert-danger':
+        return 'var(--bs-danger-bg-subtle)';
+      default:
+        return 'var(--bs-secondary-bg-subtle)';
+    }
+  };
+
+  const bgColor = backgrondColor(type);
+
   return (
-    <div className="alert alert-primary w-50 d-flex align-items-center justify-content-between" role="alert">
-      <span className="d-block">It's alert!</span>
-      <button type="button" className="btn-close d-block" aria-label="Close"></button>
+     <div className="alert mt-3 w-50 d-flex align-items-center justify-content-between"
+          style={{ visibility: alert ? 'visible' : 'hidden', backgroundColor: bgColor}} role="alert">
+      <span className="d-block">{children}</span>
+      <button type="button" className="btn-close d-block"  aria-label="Close"></button>
     </div>
+
   );
 };
 
